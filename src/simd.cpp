c4f03c2d901889118772d48a7ec5610d1aa2370c
Mateo Gray <mgray7@una.cs.uvic.ca>
2020-03-15 00:22:22 -0700

    more fixes to simd

--- src/simd.cpp
+++ src/simd.cpp
@@ -27,7 +27,7 @@ void nussinovSimd(string sequence){
 	uint16_t len = sequence.length();
 	//first we initialize the matrics D
 	uint16_t var = 0;
-	uint16_t tr1 = 8;
+	uint16_t tr1 = 16;
 	uint16_t step = 8;
 	vector< vector<uint16_t> > table(len+tr1,vector<uint16_t>(len+tr1,var));
 	
@@ -53,21 +53,22 @@ void nussinovSimd(string sequence){
 				for (uint16_t k = i+1;k<j+step-1;k+=step)
 				{
 					uint16_t n = k;
-					if(k+step> len){
-						n = len-step;
-					}
+					//if(k+step> len){
+					//	n = len-step;
+					//clear}
 					__m128i const row1 = _mm_loadu_si128( (__m128i*) &table[i][n] );
 					__m128i const row2 = _mm_loadu_si128( (__m128i*) &table[j][n + 1] );
 					__m128i result_values = _mm_add_epi16(row1,row2);
 					vector<uint16_t> temp = {-1,-1,-1,-1,-1,-1,-1,-1};
 					// printVector(temp);
+					// cout << "i " << i << " " << "j " << j << " " << "k " << k << endl;
 					__m128i tempr = _mm_loadu_si128( (__m128i*) &temp[0] );
-					// vector<uint16_t> results = {_mm_extract_epi16(result_values,0),_mm_extract_epi16(result_values,1),_mm_extract_epi16(result_values,2),_mm_extract_epi16(result_values,3),_mm_extract_epi16(result_values,4),_mm_extract_epi16(result_values,5),_mm_extract_epi16(result_values,6),_mm_extract_epi16(result_values,7)};
+					vector<uint16_t> results = {_mm_extract_epi16(result_values,0),_mm_extract_epi16(result_values,1),_mm_extract_epi16(result_values,2),_mm_extract_epi16(result_values,3),_mm_extract_epi16(result_values,4),_mm_extract_epi16(result_values,5),_mm_extract_epi16(result_values,6),_mm_extract_epi16(result_values,7)};
 					// cout << "results";
 					// cout << endl;
 					// printVector(results);
 
-					// results = {_mm_extract_epi16(tempr,0),_mm_extract_epi16(tempr,1),_mm_extract_epi16(tempr,2),_mm_extract_epi16(tempr,3),_mm_extract_epi16(tempr,4),_mm_extract_epi16(tempr,5),_mm_extract_epi16(tempr,6),_mm_extract_epi16(tempr,7)};
+					results = {_mm_extract_epi16(tempr,0),_mm_extract_epi16(tempr,1),_mm_extract_epi16(tempr,2),_mm_extract_epi16(tempr,3),_mm_extract_epi16(tempr,4),_mm_extract_epi16(tempr,5),_mm_extract_epi16(tempr,6),_mm_extract_epi16(tempr,7)};
 					// cout << "tempr";
 					// cout << endl;
 					// printVector(results);
@@ -76,22 +77,22 @@ void nussinovSimd(string sequence){
 
 
 
-					// results = {_mm_extract_epi16(xor_result_values,0),_mm_extract_epi16(xor_result_values,1),_mm_extract_epi16(xor_result_values,2),_mm_extract_epi16(xor_result_values,3),_mm_extract_epi16(xor_result_values,4),_mm_extract_epi16(xor_result_values,5),_mm_extract_epi16(xor_result_values,6),_mm_extract_epi16(xor_result_values,7)};
-					// cout << "one";
+					results = {_mm_extract_epi16(xor_result_values,0),_mm_extract_epi16(xor_result_values,1),_mm_extract_epi16(xor_result_values,2),_mm_extract_epi16(xor_result_values,3),_mm_extract_epi16(xor_result_values,4),_mm_extract_epi16(xor_result_values,5),_mm_extract_epi16(xor_result_values,6),_mm_extract_epi16(xor_result_values,7)};
+					// cout << "xor";
 					// cout << endl;
 					// printVector(results);
 					
 					__m128i min = _mm_minpos_epu16 (xor_result_values);
 					
 					xor_result_values = _mm_xor_si128(min, tempr);
-					// results = {_mm_extract_epi16(xor_result_values,0),_mm_extract_epi16(xor_result_values,1),_mm_extract_epi16(xor_result_values,2),_mm_extract_epi16(xor_result_values,3),_mm_extract_epi16(xor_result_values,4),_mm_extract_epi16(xor_result_values,5),_mm_extract_epi16(xor_result_values,6),_mm_extract_epi16(xor_result_values,7)};
-					// cout << "two";
+					results = {_mm_extract_epi16(xor_result_values,0),_mm_extract_epi16(xor_result_values,1),_mm_extract_epi16(xor_result_values,2),_mm_extract_epi16(xor_result_values,3),_mm_extract_epi16(xor_result_values,4),_mm_extract_epi16(xor_result_values,5),_mm_extract_epi16(xor_result_values,6),_mm_extract_epi16(xor_result_values,7)};
+					// cout << "xor2";
 					// cout << endl;
 					// printVector(results);
 
-					//vector<int> results = {_mm_extract_epi16(result_values,0),_mm_extract_epi16(result_values,1),_mm_extract_epi16(result_values,2),_mm_extract_epi16(result_values,3),_mm_extract_epi16(result_values,4),_mm_extract_epi16(result_values,5),_mm_extract_epi16(result_values,6),_mm_extract_epi16(result_values,7)};
-					//for(uint16_t m=0;m<8;m++){ 
-						//union { __m128i result_values; int16_t i16[8]; };
+					// vector<int> results = {_mm_extract_epi16(result_values,0),_mm_extract_epi16(result_values,1),_mm_extract_epi16(result_values,2),_mm_extract_epi16(result_values,3),_mm_extract_epi16(result_values,4),_mm_extract_epi16(result_values,5),_mm_extract_epi16(result_values,6),_mm_extract_epi16(result_values,7)};
+					// for(uint16_t m=0;m<8;m++){ 
+					// 	union { __m128i result_values; int16_t i16[8]; };
 						
 							//uint16_t value = results[m];
 							uint16_t rez = _mm_extract_epi16(xor_result_values,0);
@@ -122,7 +123,7 @@ void nussinovSimd(string sequence){
 	structure = traceback(table, 0, len-1, sequence);
 	cout << energy << endl;
 	cout << structure << endl;
-	printTable(table,sequence);
+	 //printTable(table,sequence);
 }
 
 
