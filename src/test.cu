2cc375db514d0fd156b777adc5536f1d75ad636c
Mateo Gray <mgray7@una.cs.uvic.ca>
2020-04-03 01:04:04 -0700

    changes toward multi block multi sequence

---
+++ src/test.cu
@@ -0,0 +1,30 @@
+__global__ void add(int *a, int *b, int *c) {
+    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+}
+
+
+#define N 512
+    int main(void) {
+    int *a, *b, *c; // host copies of a, b, c
+    int *d_a, *d_b, *d_c; // device copies of a, b, c
+    int size = N * sizeof(int);
+    // Alloc space for device copies of a, b, c
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_b, size);
+    cudaMalloc((void **)&d_c, size);
+    // Alloc space for host copies of a, b, c and setup input values
+    a = (int *)malloc(size); random_ints(a, N);
+    b = (int *)malloc(size); random_ints(b, N);
+    c = (int *)malloc(size);
+    // Copy inputs to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    // Launch add() kernel on GPU with N blocks
+    add<<<N,1>>>(d_a, d_b, d_c);
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    // Cleanup
+    free(a); free(b); free(c);
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    return 0;
+}
