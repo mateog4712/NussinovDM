78cd1066aae8e9f86fc45d50d82c12392e3bfec0
Hamza Iseric <hiseric1@testbed.qps14ljb0n0elhirz1v00km1oe.ux.internal.cloudapp.net>
2020-04-04 06:20:16 +0000

    Added gread paralele GPU code

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main() {
 	/** Definition of Variables and creation of data **/
 	//srand(time(0));
 	//nussinovSimd2(s12);
-	int numofIter = 1;
+	int numofIter = 5;
 	
 	// vector<string> testData;// = {"AAGGGGUU"};
 	//cout << "Creating data..." << endl;
@@ -33,20 +33,37 @@ int main() {
 	/** The functions are run **/
 
 
-	// double avg = 0.0;
+	double avg = 0.0;
+	cout << "Simd: ";
+
 	for (int j=0;j<numofIter;j++)
 	{
-		// auto start = chrono::steady_clock::now();
+		auto start = chrono::steady_clock::now();
 		for(auto i=0;i<testData.size();i++){
-			cout << "simdPar: ";
-			nussinovSimd2(testData[i]);
-			cout << "baseline: ";
-			nussinov(testData[i]);
-			cout << "simd: ";
+			
 			nussinovSimd(testData[i]);
-
 		}
+		auto end = chrono::steady_clock::now();
+		avg += chrono::duration_cast<chrono::seconds>(end-start).count();
+
 	}
+	avg = avg / numofIter;
+	cout << "Average elapsed time (SIMD): " << avg << endl;
+
+	avg = 0.0;
+	cout << "Optimized: ";
+	for (int j=0;j<numofIter;j++)
+	{
+		auto start = chrono::steady_clock::now();
+		for(auto i=0;i<testData.size();i++){
+			cout << "Simd: ";
+			nussinovOpt(testData[i]);
+		}
+		auto end = chrono::steady_clock::now();
+		avg += chrono::duration_cast<chrono::seconds>(end-start).count();
+	}
+	avg = avg / numofIter;
+	cout << "Average elapsed time (Optimized single core): " << avg << endl;
 	// 	auto end = chrono::steady_clock::now();
 	// 	avg += chrono::duration_cast<chrono::seconds>(end-start).count();
 	// }
